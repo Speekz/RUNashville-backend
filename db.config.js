da8fd447f6f077f0452406c127d3ee9dace8b8ab
Johan Bejar <johanbejarc@hotmail.com>
2021-04-09 13:46:06 -0400

    Discarted Heroku, now going for EC2

--- db.config.js
+++ db.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  USER: "baf949ecaee65c",
-  PASSWORD: "ed9f68b9",
-  HOST: "us-cdbr-east-03.cleardb.com",
-  DB: "heroku_3eb998019afca04"
+  USER: "",
+  PASSWORD: "",
+  HOST: "",
+  DB: ""
 };
-
-// mysql --host=us-cdbr-east-03.cleardb.com --user=baf949ecaee65c --password=ed9f68b9 --reconnect < db/schema.sql
